191bc8909a351ef5760b111b58a312b598c9812c
Richard Mai <richardmai@Richards-MacBook-Pro.local>
2019-07-06 13:42:53 -0400

    Created react components and info area and test area

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,50 +1,57 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-
+import InfoArea from './infoarea';
+import TestArea from './testarea';
 
 class Landing extends Component {
   render() {
-    return(
-      <div style={{width: '100%', margin: 'auto'}}>
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img
+            {/* <img
               src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
               alt="avatar"
               className="avatar-img"
-              />
+            /> */}
 
             <div className="banner-text">
-              <h1>Full Stack Web Developer</h1>
+              <h1>Learning App</h1>
 
-            <hr/>
+              <hr />
+              <br />
 
-          <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+              <p>Learn HTML | CSS | JS | {'& More Coming Soon'} </p>
 
-        <div className="social-links">
+              <div className="social-links">
 
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
+                {/* LinkedIn */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
 
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
+                {/* Github */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
 
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
+                {/* Freecodecamp */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
 
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
+                {/* Youtube */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
 
-        </div>
+              </div>
             </div>
+            
+            <InfoArea/>
+
+            <TestArea/>
+
           </Cell>
         </Grid>
       </div>
