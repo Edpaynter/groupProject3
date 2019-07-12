158aa1c850b6bd5dc57ac92613d13251daaab184
Steven John Cantando <stevenjohncantando@gmail.com>
2019-07-11 20:15:56 -0400

    fixed merged conflict

--- src/components/testarea.js
+++ src/components/testarea.js
@@ -4,11 +4,7 @@ class TestArea extends Component {
     render() {
         return (
             <div className="banner-text">
-<<<<<<< HEAD
-              <h1>Test Area</h1>
-=======
               <h1>CSS</h1>
->>>>>>> a767180... questions uploaded
               <hr/>
               <br/>
               <p>CSS stands for "Cascading Style Sheet." Cascading style sheets are used to format the layout of Web pages. They can be used to define text styles, table sizes, and other aspects of Web pages that previously could only be defined in a page's HTML.</p>
