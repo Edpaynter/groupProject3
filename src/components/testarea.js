563c37230a169997b0b4aac5294d364431088f81
Steven John Cantando <stevenjohncantando@gmail.com>
2019-07-17 20:51:51 -0400

    css changes and comments

--- src/components/testarea.js
+++ src/components/testarea.js
@@ -1,16 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import quizDictionary from './quizQuestions';
+import Nextbutton from './nextButton';
+import Backbutton from './previousButton';
 
 class TestArea extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increment: 0,
+        };
+    }
+
+    IncrementItem = () => {
+        if (this.state.increment < quizDictionary.CSS.quiz_questions.length - 1) {
+            this.setState({ increment: this.state.increment + 1 });
+        } else {
+            this.setState({ increment: 0 });
+        }
+
+    }
+
+    DecrementItem = () => {
+        if (this.state.increment > 0) {
+            this.setState({ increment: this.state.increment - 1 });
+        } else {
+            this.setState({ increment: 0 });
+        }
+
+    }
+
+
+
     render() {
         return (
-            <div className="banner-text">
-              <h1>CSS</h1>
-              <hr/>
-              <br/>
-              <p>CSS stands for "Cascading Style Sheet." Cascading style sheets are used to format the layout of Web pages. They can be used to define text styles, table sizes, and other aspects of Web pages that previously could only be defined in a page's HTML.</p>
+
+            <div style={{ background: 'rgba(0, 0, 0, 0.3)' }} className="banner-text">
+                <h1 style={{ fontSize: 60, fontFamily: "Tahoma", fontWeight: 'normal' }} >{quizDictionary.CSS.name}</h1>
+                <p> ____________________________________________________________________</p>
+                <p style={{ fontSize: 40, fontFamily: "Gill sans", fontWeight: 'normal' }}>{quizDictionary.CSS.quiz_questions[this.state.increment].quiz}</p>
+                <h1 style={{ fontSize: 30, fontFamily: "Gill sans", fontWeight: 'normal' }}>{quizDictionary.CSS.quiz_questions[this.state.increment].answer}</h1>
+
+
+                <Grid className="demo-grid-2">
+                    <Cell col={5}><Backbutton onClick={this.DecrementItem} increment={this.state.increment} ></Backbutton></Cell>
+                    <Cell col={3}><Nextbutton onClick={this.IncrementItem} increment={this.state.increment}></Nextbutton> </Cell>
+                </Grid>
+
             </div>
         )
     }
 }
+
 
 export default TestArea;
